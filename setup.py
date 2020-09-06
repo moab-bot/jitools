d0536ce59fcb64ea2c8fafd44619c7e8a19b7da9
M.O. Abbott <moab_bot@protonmail.com>
2020-09-06 23:46:42 +0200

    fixed setup.py for pypi

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setuptools.setup(
     author="M.O. Abbott",
     author_email="moab_bot@protonmail.com",
     description="a Python-based set of utilities for just intonation (JI) pitch and pitch collection research and analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/moab-bot/jitools",
     download_url = 'https://github.com/moab-bot/jitools/archive/v_01.tar.gz',
     packages=setuptools.find_packages(),
